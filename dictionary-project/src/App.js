f861e50ab76ae17bb15e879bee226482e6eb7f05
miszindriasari <ti.tahsin@hotmail.com>
2023-05-22 13:36:14 +0200

    Update App.js

--- dictionary-project/src/App.js
+++ dictionary-project/src/App.js
@@ -6,10 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
-        <a href="#" className="btn btn-primary">
-          Hello
-        </a>
       </header>
     </div>
   );
